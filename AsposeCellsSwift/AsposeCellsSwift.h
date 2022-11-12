852df10661c83c02ac1740816908393d964aa6f7
Wo_0NDeR ™ <fidelhernandezsalazar@gmail.com>
2022-11-12 15:55:19 -0500

    Initial Commit

---
+++ AsposeCellsSwift/AsposeCellsSwift.h
@@ -0,0 +1,18 @@
+//
+//  AsposeCellsSwift.h
+//  AsposeCellsSwift
+//
+//  Created by Fidel Hernández Salazar on 11/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AsposeCellsSwift.
+FOUNDATION_EXPORT double AsposeCellsSwiftVersionNumber;
+
+//! Project version string for AsposeCellsSwift.
+FOUNDATION_EXPORT const unsigned char AsposeCellsSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsposeCellsSwift/PublicHeader.h>
+
+
