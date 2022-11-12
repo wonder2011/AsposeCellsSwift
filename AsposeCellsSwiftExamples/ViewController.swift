124c4819768fd835d52cc84b42298153ab6f91df
Wo_0NDeR ™ <fidelhernandezsalazar@gmail.com>
2022-11-12 17:02:01 -0500

    Add Added AsposeCellsSwift.podspec

---
+++ AsposeCellsSwiftExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AsposeCellsSwiftExamples
+//
+//  Created by Fidel Hernández Salazar on 11/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Enums/CellValueType.swift
@@ -0,0 +1,14 @@
+//
+//  CellValueType.swift
+//  AsposeCellsSwift
+//
+//  Created by Fidel Hernández Salazar on 11/12/22.
+//
+
+import Foundation
+
+enum CellValueType: String {
+    case string = "string"
+    case int = "int"
+    // TODO: Add other types when needed
+}
