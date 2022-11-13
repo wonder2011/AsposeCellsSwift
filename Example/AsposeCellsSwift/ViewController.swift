847debe39c6e7ee12bbe8c9f90d0f601d1df2fd3
Wo_0NDeR ™ <fidelhernandezsalazar@gmail.com>
2022-11-12 21:20:53 -0500

    Update ViewController.swift

--- Example/AsposeCellsSwift/ViewController.swift
+++ Example/AsposeCellsSwift/ViewController.swift
@@ -11,15 +11,44 @@ import AsposeCellsSwift
 
 class ViewController: UIViewController {
 
+    let clientId = "fc3d9b24-3513-4607-9ba4-b807231cc019"
+    let clientSecret = "cafec20eae1dd7f28a52da033996bc4c"
+
+    let storageName = "Lionex iOS App"
+    let fileName = "test.xlsx"
+    let worksheetName = "Start"
+
+    var asposeCellWrapper: AsposeCellsSwift!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.asposeCellWrapper = AsposeCellsSwift(clientId: self.clientId, clientSecret: self.clientSecret)
+
+        self.usageExample()
     }
+
+    /*
+     Some usage examples of this class
+     */
+    func usageExample() {
+
+        Task {
+            do {
+                let _ = try await self.asposeCellWrapper.authorizeClient()
+            } catch(let error) {
+                print(error)
+            }
+        }
+    }
+
+}
+
+extension ViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 }
 
